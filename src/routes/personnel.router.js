7954378d2d73ab9aaa85363ccd2139fb42421e9a
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-03-05 19:58:21 +0300

    Update personnel.router.js

--- src/routes/personnel.router.js
+++ src/routes/personnel.router.js
@@ -1,27 +1,21 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
-
-const personnel = require('../controllers/personnel.controller')
+const personnel = require("../controllers/personnel.controller");
 
 // URL: /personnels
 
-// Login/logout:
-router.post('/login', personnel.login)
-router.all('/logout', personnel.logout)
+router.route("/").get(personnel.list).post(personnel.create);
 
-router.route('/')
-    .get(personnel.list)
-    .post(personnel.create)
-
-router.route('/:id')
-    .get(personnel.read)
-    .put(personnel.update)
-    .patch(personnel.update)
-    .delete(personnel.delete)
+router
+  .route("/:id")
+  .get(personnel.read)
+  .put(personnel.update)
+  .patch(personnel.update)
+  .delete(personnel.delete);
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
